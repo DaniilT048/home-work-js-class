e26b1d161f10b623febd840df6764b90252abab6
DaniilT048 <tis.daniil98@gmail.com>
2025-02-03 07:34:45 +0200

    task a

--- Human.js
+++ Human.js
@@ -1,2 +1,13 @@
 'use strict';
 
+class Humans {
+    constructor(name, gender) {
+        this.name = name;
+        this.gender = gender;
+    }
+    person() {
+        console.log (`Имя:${this.name} Пол:${this.gender}`)
+    }
+}
+
+export {Humans};
